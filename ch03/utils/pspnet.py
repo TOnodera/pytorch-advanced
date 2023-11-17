8ab961d7905b16a2204d048d4984ca503e2ac741
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-11-17 19:52:32 +0900

    3-5まで実装

--- ch03/utils/pspnet.py
+++ ch03/utils/pspnet.py
@@ -1,4 +1,6 @@
 from torch import nn
+import torch.nn.functional as F
+import torch
 
 class PSPNet(nn.Module):
     def __init__(self, n_classes):
@@ -153,3 +155,51 @@ class bottleNeckIdentifyPSP(nn.Module):
         residual = x
         return self.relu(conv + residual)
         
+
+class PyramidPooling(nn.Module):
+    def __init__(self, in_channels, pool_size, height, width):
+        super(PyramidPooling, self).__init__()
+        self.height = height
+        self.widht = width
+        
+        out_channels = int(in_channels/len(pool_size))
+        
+        # pool_size = [6, 3, 2, 1]
+        self.avpool_1 = nn.AdaptiveAvgPool2d(output_size=pool_size[0])
+        self.cbr_1 = conv2DBatchNormRelu(
+            in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+
+        self.avpool_2 = nn.AdaptiveAvgPool2d(output_size=pool_size[1])
+        self.cbr_2 = conv2DBatchNormRelu(
+            in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+
+        self.avpool_3 = nn.AdaptiveAvgPool2d(output_size=pool_size[2])
+        self.cbr_3 = conv2DBatchNormRelu(
+            in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+
+        self.avpool_4 = nn.AdaptiveAvgPool2d(output_size=pool_size[3])
+        self.cbr_4 = conv2DBatchNormRelu(
+            in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+        
+
+    def forward(self, x):
+        out1 = self.cbr_1(self.avpool_1(x))
+        out1 = F.interpolate(out1, size=(self.height, self.widht), mode="bilinear",align_corners=True)
+        
+        out2 = self.cbr_2(self.avpool_2(x))
+        out2 = F.interpolate(out2, size=(self.height, self.widht), mode="bilinear",align_corners=True)
+        
+        out3 = self.cbr_3(self.avpool_3(x))
+        out3 = F.interpolate(out3, size=(self.height, self.widht), mode="bilinear",align_corners=True)
+
+        out4 = self.cbr_4(self.avpool_4(x))
+        out4 = F.interpolate(out4, size=(self.height, self.widht), mode="bilinear",align_corners=True)
+        
+        # 最終的に結合させる
+        output = torch.cat([x, out1, out2, out3, out4], dim=1)
+        
+        return output
